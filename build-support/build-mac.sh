bb84803fbf5cc3a58d2b75a485e8ba3f3c23478b
Fretiq <60456830+fretiq@users.noreply.github.com>
2020-05-27 19:39:17 -0500

    Readd sysroot

--- build-support/build-mac.sh
+++ build-support/build-mac.sh
@@ -25,7 +25,7 @@ clientdir="${tmpdir}/apache-pulsar-2.5.2/pulsar-client-cpp/"
 
 pushd "${clientdir}"
 
-cmake . -DCMAKE_C_FLAGS_RELEASE=-DNDEBUG -DCMAKE_CXX_FLAGS_RELEASE=-DNDEBUG -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_FRAMEWORK=LAST -DCMAKE_VERBOSE_MAKEFILE=ON -Wno-dev -DBUILD_TESTS=OFF -DLINK_STATIC=ON -DBUILD_PYTHON_WRAPPER=OFF -DBoost_INCLUDE_DIRS=/usr/local/opt/boost/include -DCURL_LIBRARIES=/usr/local/opt/curl/lib/libcurl.a
+cmake . -DCMAKE_C_FLAGS_RELEASE=-DNDEBUG -DCMAKE_CXX_FLAGS_RELEASE=-DNDEBUG -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_FRAMEWORK=LAST -DCMAKE_VERBOSE_MAKEFILE=ON -Wno-dev -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk -DBUILD_TESTS=OFF -DLINK_STATIC=ON -DBUILD_PYTHON_WRAPPER=OFF -DBoost_INCLUDE_DIRS=/usr/local/opt/boost/include -DCURL_LIBRARIES=/usr/local/opt/curl/lib/libcurl.a
 
 make pulsarStaticWithDeps -j 3
 
